d30b4614d3adbeae3d2c6ce049dd4c6e9b3488b6
vishalsanfran <vkhawarey@maprtech.com>
2017-09-10 10:03:00 -0700

    autocomplete support

--- trie/trie_class.rb
+++ trie/trie_class.rb
@@ -1,7 +1,9 @@
 class TrieNode
-  attr_accessor :children
-  def initialize
+  attr_accessor :children, :val, :isLast
+  def initialize(val=nil, isLast=false)
+    @val = val
     @children = {}
+    @isLast = isLast
   end
 end
 
@@ -16,13 +18,16 @@ class PrefixTrie
     while cnt < size
       if curNode.children.include? word[cnt]
         curNode = curNode.children[word[cnt]]
+        curNode.isLast = cnt == size -1? true: false
+        puts "has cur #{curNode.val} #{curNode.isLast.inspect}"
         cnt += 1
       else
         break
       end
     end
     while cnt < size
-      curNode.children[word[cnt]] = TrieNode.new
+      isLast = cnt == size -1? true: false
+      curNode.children[word[cnt]] = TrieNode.new(word[cnt], isLast)
       curNode = curNode.children[word[cnt]]
       cnt += 1
     end
@@ -41,14 +46,43 @@ class PrefixTrie
     end
     return true
   end
-  def search(word)
-    
+  def startsWithChildren(word)
+    curNode = @root
+    cnt = 0
+    while cnt < word.length
+      curNode = curNode.children[word[cnt]]
+      if curNode == nil
+        return []
+      end
+      cnt += 1
+    end
+    arry = getChildrenArray(curNode)
+    arry.map{|sfx| word + sfx}
+  end
+  def getChildrenArray(node)
+    res = []
+    dfs(node, res, [])
+    res
+  end
+  def dfs(node, res, cur)
+    puts "cur #{node.val} #{node.isLast.inspect}"
+    if node.children.empty? || node.isLast
+      res.push(cur.join())
+    end
+    node.children.each do |key, childNode|
+      cur.push(key)
+      dfs(childNode, res, cur)
+      cur.pop()
+    end
   end
 end
 
 pt = PrefixTrie.new
+pt.insert("homez")
 pt.insert("home")
-pt.insert("hole")
 pt.insert("homer")
+pt.insert("hole")
+
 puts "\tDoes it include 'hom'? : #{pt.startsWith('hom')}\n
   \tDoes it include 'homs'? : #{pt.startsWith('homs')}"
+puts "All children of 'hom': #{pt.startsWithChildren('hom')}"
